8f6a54c64a826e2a0fe13752e53f3c03f3930ed5
Marc Ignacio <marcrendlignacio@gmail.com>
2013-08-08 00:17:34 +0800

    first iteration of word-count exercise

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,11 +1,24 @@
 class Phrase
-  attr_reader :text
-
   def initialize(text)
     @text = text
   end
 
   def word_count
-    {text => 1}
+    words.reduce({}) do |result, word|
+      count_towards result, word
+    end
+  end
+
+  def count_towards result, word
+    if word_count = result[word]
+      result[word] = word_count + 1
+    else
+      result.merge!(word => 1)
+    end
+    result
+  end
+
+  def words
+    @text.downcase.gsub(/\W/,' ').split
   end
 end

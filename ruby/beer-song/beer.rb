7bff6050212a68e8d4871556363db9d2a0c031db
Marc Ignacio <marcrendlignacio@gmail.com>
2013-08-19 12:32:18 +0800

    ruby: beer song - first iteration

--- ruby/beer-song/beer.rb
+++ ruby/beer-song/beer.rb
@@ -1,28 +1,41 @@
 class Beer
-  def verse number_of_beers
-    result = first_line(number_of_beers)
-    result << second_line(number_of_beers)
+  def sing(start, finish=0)
+    start.downto(finish).each_with_object("") do |number_of_beers, result|
+      result << verse(number_of_beers)
+      result << "\n"
+    end
+  end
+
+  def verse(number_of_beers)
+    result = first_line(number_of_beers).capitalize
+    result << second_line(number_of_beers).capitalize
     result
   end
 
-  def first_line number_of_beers
+  def first_line(number_of_beers)
     "#{bottles(number_of_beers)} of beer on the wall, #{bottles(number_of_beers)} of beer.\n"
   end
 
-  def second_line number_of_beers
-    "Take #{the_beer(number_of_beers)} down and pass it around, #{beers_left(number_of_beers)} of beer on the wall.\n"
+  def second_line(number_of_beers)
+    first_sentence = if number_of_beers == 0
+      "Go to the store and buy some more,"
+    else
+      "Take #{the_beer(number_of_beers)} down and pass it around,"
+    end
+    first_sentence + " #{beers_left(number_of_beers)} of beer on the wall.\n"
   end
 
   private
 
-  def bottles number_of_beers
+  def bottles(number_of_beers)
     case number_of_beers
+    when 0 then "no more bottles"
     when 1 then "1 bottle"
     else "#{number_of_beers} bottles"
     end
   end
 
-  def the_beer number_of_beers
+  def the_beer(number_of_beers)
     case number_of_beers
     when 1 then "it"
     else "one"
@@ -30,7 +43,7 @@ class Beer
   end
 
   def beers_left(number_of_beers)
-    number_of_beers_left = number_of_beers - 1
+    number_of_beers_left = (number_of_beers - 1) % 100
     case number_of_beers_left
     when 0 then "no more bottles"
     when 1 then "1 bottle"
